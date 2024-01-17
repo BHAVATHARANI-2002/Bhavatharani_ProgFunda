a8ab1c70d68953119ea986fafdeeac606eeb0615
BHAVATHARANI-2002 <154747263+BHAVATHARANI-2002@users.noreply.github.com>
2024-01-17 19:43:12 +0530

    Add files via upload

---
+++ Assignment/src/com/gradedproject/q1/AdminDepartment.java
@@ -0,0 +1,25 @@
+package com.gradedproject.q1;
+
+public class AdminDepartment extends SuperDepartment
+{
+		//creating method using departmentaname
+		// override the method
+		public String departmentName()
+		{
+			return"Admin Department";
+		}
+		//creating method using method name getTodaysWork
+		// override the method
+		public String getTodaysWork()
+
+		{
+			return "Complete your documents Submission";
+		}
+		//creating method using method name getWorkDeadline
+		// override the method
+		public String getWorkDeadline()
+		{
+			return "complete by EOD";
+		}
+	}
+
---
+++ Assignment/src/com/gradedproject/q1/SuperDepartment.java
@@ -0,0 +1,25 @@
+package com.gradedproject.q1;
+
+public class SuperDepartment 
+{
+	//creating a method name departmentName
+		public String departmentName()
+		{
+			return "super Department";
+		}
+		//creating a method name getTodayswork
+		public String getTodaysWork()
+		{
+			return "No Work as of now";
+		}
+		//creating a method name getworkDeadline
+		public String getworkDeadline() 
+		{
+			return "Nill" ;
+		}
+		//creating a method name isTodayAHoliday
+		public String isTodayAHoliday()
+		{
+			return "Today is not a holiday";
+		}
+}
